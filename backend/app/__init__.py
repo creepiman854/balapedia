a8073bff0bf9ff257f01f6ac24c59a0dcedc4cdb
Creepi <92860393+creepiman854@users.noreply.github.com>
2026-05-30 21:36:38 +0200

    fix(cors): update cors configuration to support credentials and specific origins

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -14,7 +14,11 @@ def create_app(config_class=DevConfig):
     # Extensiones
     db.init_app(app)
     migrate.init_app(app, db)
-    cors.init_app(app, origins=app.config["CORS_ORIGINS"])
+    cors.init_app(
+        app,
+        resources={r"/api/*": {"origins": app.config["CORS_ORIGINS"]}},
+        supports_credentials=True,
+    )
     limiter.init_app(app)
     mail.init_app(app)
 
